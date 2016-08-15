a2076cbdb7f66797c01129ed89f2fd7cce369559
Pat Driscoll <patrick.driscoll@my.jcu.edu.au>
2016-08-15 10:46:54 +1000

    Add temperarureV2.py

--- Practical01/Practical03/temperatureV2.py
+++ Practical01/Practical03/temperatureV2.py
@@ -1,26 +1,41 @@
 """
 CP1404/CP5632 - Practical
-Pseudocode for temperature conversion
+Modified from Practical01\temperature.py using refactoring for functions
 """
 __author__ = 'Lindsay Ward'
 
-MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        # TODO: Write this section so the program converts F to C and displays the result
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        farenheit = float(input("Farenheit: "))
-        celsius = 5 / 9 * (farenheit - 32)
-        print("Result: {:.2f} C".format(celsius))
-        pass
-    else:
-        print("Invalid option")
+
+def main():
+
+    MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            fahrenheit = celsius_to_farenheit()
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+            celsius = farenheit_to_celsius()
+            print("Result: {:.2f} C".format(celsius))
+            pass
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def farenheit_to_celsius():
+    farenheit = float(input("Farenheit: "))
+    celsius = 5 / 9 * (farenheit - 32)
+    return celsius
+
+
+def celsius_to_farenheit():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+main()

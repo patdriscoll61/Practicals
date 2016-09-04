7e119208fdf58e206195e49f021e36b6f584bee5
Pat Driscoll <patrick.driscoll@my.jcu.edu.au>
2016-09-04 19:54:25 +1000

    milesToKm stage 2

--- Practical01/Practical06/milesToKm_2.py
+++ Practical01/Practical06/milesToKm_2.py
@@ -12,6 +12,7 @@ __author__ = 'Patrick Driscoll'
 
 class MilesToKmApp(App):
     """ MilesToKmApp is a Kivy App for converting Miles to KM """
+
     def build(self):
         """ build the Kivy app from the kv file """
         Window.size = (500, 200)
@@ -19,14 +20,20 @@ class MilesToKmApp(App):
         self.root = Builder.load_file('milesToKm_2.kv')
         return self.root
 
-    def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
-        result = value * 1.609344
-        self.root.ids.output_label.text = str(result)
+    def handle_calculate(self, str_value):
+        """ handle calculation (could be button press or other call), output result to label widget
+            check if the input is valid first, if not set output to 0.0 """
+        try:
+            value = int(str_value)
+            result = value * 1.609344
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            self.root.ids.output_label.text = '0.0'
+            self.root.ids.text_input.text = ''
 
     def handle_increment(self, increment, ):
         if self.root.ids.text_input.text == '':
-            self.root.ids.text_input.text  = '0'
+            self.root.ids.text_input.text = '0'
         self.root.ids.text_input.text = str(int(self.root.ids.text_input.text) + increment)
         self.handle_calculate(int(self.root.ids.text_input.text))
 
